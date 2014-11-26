36dec9157c8483a6b118f64d3912ccb272b6316d
David Ladowitz <david@ladowitz.com>
2014-11-25 16:27:41 -0800

    meal updating warnings

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -33,38 +33,68 @@ class Meal < ActiveRecord::Base
     return false
   end
 
-  def show_warning?
-    if ignore_warnings?
-      false
+  def show_warning?(warning_type = nil)
+    return false if ignore_warnings?
+
+    if warning_type
+      option_warning?(warning_type)
     else
       has_warnings? ? true : false
     end
+
+    # case warning_type
+    # when :gluten_free
+    #   gluten_free_warning?
+    # when :vegetarian
+    #   vegetarian_warning?
+    # when :vegan
+    #   vegan_warning?
+    # when nil
+    #   has_warnings? ? true : false
+    # end
   end
 
-  def missing_options
-    #TODO refactor
-
-    event_options = []
-    event_options << "Gluten-Free" if event.gluten_free
-    event_options << "Vegetarian"  if event.gluten_free
-    event_options << "Vegan"       if event.vegan
-
-    meal_options =  []
-    dishes.each do |dish|
-      meal_options << "Gluten-Free" if dish.gluten_free
-      meal_options << "Vegetarian"  if dish.vegetarian
-      meal_options << "Vegan"       if dish.vegan
-    end
-
-    meal_options.uniq!
-
-    missing_options = []
-
-    event_options.each { |option| missing_options << option unless meal_options.include?(option) }
-    missing_options
-  end
+  # def missing_options
+  #   #TODO refactor
+  #
+  #   event_options = []
+  #   event_options << "Gluten-Free" if event.gluten_free
+  #   event_options << "Vegetarian"  if event.gluten_free
+  #   event_options << "Vegan"       if event.vegan
+  #
+  #   meal_options =  []
+  #   dishes.each do |dish|
+  #     meal_options << "Gluten-Free" if dish.gluten_free
+  #     meal_options << "Vegetarian"  if dish.vegetarian
+  #     meal_options << "Vegan"       if dish.vegan
+  #   end
+  #
+  #   meal_options.uniq!
+  #
+  #   missing_options = []
+  #
+  #   event_options.each { |option| missing_options << option unless meal_options.include?(option) }
+  #   missing_options
+  # end
 
   private
+
+  # def gluten_free_warning?
+  #   self.event.gluten_free? && !dishes_include_option?(:gluten_free)
+  # end
+
+  def option_warning?(option_type)
+    # checks if event require option type and none of the dishes include this type
+    self.event.send(option_type) && !dishes_include_option?(option_type)
+  end
+
+  def dishes_include_option?(option_type)
+    self.dishes.each do |dish|
+      return true if dish.send(option_type) #calling option_type as a attribute
+    end
+
+    false
+  end
 
   def valid_category?
     unless VALID_CATEGORIES.include? category
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -56,47 +56,83 @@ describe Meal do
   end
 
   describe "#show_warning?" do
-    subject { meal.show_warning? }
+    context "when the event requires gluten-free options" do
+      before { meal.event.update gluten_free: true }
 
-    context "when a dish has warnings" do
+      context "when no dishes in the meal are gluten-free" do
+        let(:warning_type) { :gluten_free}
+        before { dish.update gluten_free: false }
+
+        context "when ignore_warnings returns true" do
+          before { meal.update ignore_warnings: true }
+
+          it "returns true" do
+            expect(meal.show_warning?(:gluten_free)).to be false
+          end
+        end
+
+        context "when ignore_warnings returns false" do
+          before { meal.update ignore_warnings: false }
+
+          it "returns true" do
+            expect(meal.show_warning?(:gluten_free)).to be true
+          end
+        end
+      end
+    end
+
+    context "when the event does not require gluten-free options" do
+      before { meal.event.update gluten_free: false }
+
+      context "when no dishes in the meal are gluten-free" do
+        let(:warning_type) { :gluten_free}
+        before { dish.update gluten_free: false }
+
+        it "returns true" do
+          expect(meal.show_warning?(:gluten_free)).to be false
+        end
+      end
+    end
+
+    context "when a dish in the meal has warnings" do
+      context "when 'ignore_warnings' is false" do
+        it "returns true" do
+          allow_any_instance_of(Dish).to receive(:has_warnings?).and_return(true)
+          expect(meal.show_warning?).to be true
+        end
+      end
+
       context "when 'ignore_warnings' is true" do
         before { meal.update ignore_warnings: true }
 
         it "returns false" do
           allow_any_instance_of(Dish).to receive(:has_warnings?).and_return(true)
-          expect(subject).to be false
-        end
-      end
-
-      context "when 'ignore_warnings' is false" do
-        it "returns true" do
-          allow_any_instance_of(Dish).to receive(:has_warnings?).and_return(true)
-          expect(subject).to be true
+          expect(meal.show_warning?).to be false
         end
       end
     end
   end
 
-  describe "#missing_options" do
-    subject { meal2.missing_options }
-    let!(:event2) { create :event, gluten_free: true, vegetarian: true, vegan: true }
-    let!(:meal2)  { create :meal, event: event2 }
-
-    context "when the meal is missing options" do
-      before { create :dish, meal: meal2 }
-
-      it "returns missing options" do
-        expect(subject).to match_array ["Gluten-Free", "Vegan"]
-      end
-    end
-
-    context "when the meal is not missing options" do
-      before { create :dish, meal: meal2, gluten_free: true, vegetarian: true, vegan: true }
-
-      it "returns an empty array" do
-        expect(subject).to match_array []
-
-      end
-    end
-  end
+  # describe "#missing_options" do
+  #   subject { meal2.missing_options }
+  #   let!(:event2) { create :event, gluten_free: true, vegetarian: true, vegan: true }
+  #   let!(:meal2)  { create :meal, event: event2 }
+  #
+  #   context "when the meal is missing options" do
+  #     before { create :dish, meal: meal2 }
+  #
+  #     it "returns missing options" do
+  #       expect(subject).to match_array ["Gluten-Free", "Vegan"]
+  #     end
+  #   end
+  #
+  #   context "when the meal is not missing options" do
+  #     before { create :dish, meal: meal2, gluten_free: true, vegetarian: true, vegan: true }
+  #
+  #     it "returns an empty array" do
+  #       expect(subject).to match_array []
+  #
+  #     end
+  #   end
+  # end
 end
