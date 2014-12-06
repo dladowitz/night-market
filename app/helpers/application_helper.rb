bf9ea9d78b398d4c33d5241d1e139d4de83b41f0
David Ladowitz <david@ladowitz.com>
2014-12-06 07:00:30 -0800

    adding todo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
+
+  #TODO look at side by side date/time pickers: http://jsfiddle.net/praveen_jegan/6FcBT/1/
   def bootstrap_datetime_format(ruby_datetime)
     datetime = ruby_datetime ? ruby_datetime : DateTime.now
     datetime.strftime("%m/%d/%Y %l:%M %p")
   end
-
 end
